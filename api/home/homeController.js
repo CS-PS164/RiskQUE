593ab5a764a6786318ffa2d479fcfb0661119bda
ferdiany19 <ferdianyuliansyah19@gmail.com>
2023-06-16 19:58:36 +0700

    add search api

--- api/home/homeController.js
+++ api/home/homeController.js
@@ -9,7 +9,15 @@ const getScoreData = async (req, res) => {
     req.headers.token,
   ]);
 
-  const id = querySession[1];
+  // if(querySession == undefined) {
+  //   const resErr = {
+  //     rc: "01",
+  //     message: "Success Update User data",
+  //   };
+  // }
+
+  const id = querySession[0].user_id;
+  console.info(id);
 
   const queryUpdateUser = "UPDATE `users` SET `tipe_risiko` = ? WHERE `id` = ?";
   const updateUser = await db.query(queryUpdateUser, [tipe_risiko, id]);
@@ -17,8 +25,10 @@ const getScoreData = async (req, res) => {
   const response = {
     rc: "01",
     message: "Success Update User data",
-    data: {},
+    data: updateUser,
   };
+
+  return res.status(200).json(response);
 };
 
 const getDplkData = async (req, res) => {
@@ -59,7 +69,56 @@ const getDplkDataById = async (req, res) => {
   } catch (error) {}
 };
 
+const searchDplk = async (req, res) => {
+  try {
+    const token = req.headers.token;
+
+    const querySearchStatement =
+      "SELECT dplk.dplk, session.token, session.token_exp FROM `dplk` INNER JOIN `session` ON dplk.id = session.user_id WHERE session.token = ?";
+    const querySearch = await db.query(querySearchStatement, [token]);
+
+    if (querySearch[0] === undefined) {
+      const resErr = {
+        rc: "13",
+        message: "Data tidak ditemukan",
+      };
+      return res.status(400).json(resErr);
+    }
+
+    if (querySearch[0].token_exp > moment().format("YYYY-MM-DD HH:mm:ss")) {
+      const resErr = {
+        rc: "63",
+        message: "Masa token telah habis",
+      };
+      return res.status(400).json(resErr);
+    }
+
+    let dplkName = "%" + (req.body.name ?? "") + "%";
+
+    const queryDplkStatement = "SELECT `dplk` FROM `dplk` WHERE `dplk` LIKE ?";
+    const queryDplk = await db.query(queryDplkStatement, [
+      dplkName.toLowerCase(),
+    ]);
+    if (getMeal[0] !== undefined) {
+      const response = {
+        rc: "00",
+        message: "Berhasil get data",
+        data: queryDplk,
+      };
+      return res.status(200).json(response);
+    } else {
+      const response = {
+        rc: "00",
+        message: "Berhasil get data",
+        data: [],
+      };
+      return res.status(200).json(response);
+    }
+  } catch (error) {}
+};
+
 module.exports = {
   getDplkData,
   getScoreData,
+  searchDplk,
 };
